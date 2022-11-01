68e33f8a35f0ed6c98f1aaa58bf3ca0c4127ce24
thadeus <thadeusnyariki94@gmail.com>
2022-11-01 14:41:53 +0300

    Not done

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
     @id = id
@@ -41,15 +42,40 @@ class Student
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
+
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
-  # def self.create(name, grade)
-  #   student = Student.new
-  #   student.save
-  # end
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    # self.new is equivalent to Student.new
+    self.new(id: row[0], name: row[1], grade: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first # grabbing the #first element from the returned array
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 
 end
